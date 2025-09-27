acea987a868ce90cc95ebbaad3296c6cc37a5557
NguyenNgocQuynhNhi <quynhnhi7bpct@gmail.com>
2025-09-27 14:08:47 +0700

    [bug]Chapter5-Exericse23: Egg-timer Program

--- Chapter5/Exercise23.cpp
+++ Chapter5/Exercise23.cpp
@@ -1,13 +1,84 @@
 /*
- 5.24 (Modified Egg-timer Program) Modify the application you wrote in Exercise 5.23 to read
- an odd number in the range 1 to 29 to specify the height of the egg-timer. Your program should
- then display an egg-timer of the appropriate size. (The egg-timer shown above in Exercise 5.23 has
- a height of 9).
+ 5.23 (Egg-timer Program) Write an application that prints the following egg-timer shape. You
+ may use output statements that print a single asterisk (*), a single hash (#) a single space or a single
+ newline character. Maximize your use of iteration (with nested for statements), and minimize the
+ number of output statements.
+
+  #########
+  #*******#
+  # ***** #
+  #  ***  #
+  #   *   #  
+  #  ***  #
+  # ***** #
+  #*******#
+  #########
+
 */
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main() {
-    
+    unsigned int height = 9;
+    unsigned int asterisk_size = height - 2;
+
+    for (int i = 0; i < height; i++) {
+        if (i == 0 || i == height - 1) {
+            for (int j = 0; j < height; j++) {
+                cout << '#';
+            }
+        }
+        else {
+            cout << '#';
+
+            int spacesN = min(i, (int)asterisk_size - 1 - i);
+            for (int space = 0; space < spacesN; space++) {
+                cout << ' ';
+            }
+
+            unsigned int asterisksN = height - spacesN * 2;
+            for (int asterisk = 0; asterisk < asterisksN; asterisk++) {
+                cout << '*';
+            }
+
+            for (int space = 0; space < spacesN; space++) {
+                cout << ' ';
+            }
+
+            cout << '#';
+        }
+
+        cout << '\n';
+    }
+
+    // for (int i = 0; i < height; i++) {
+    //     if (i == 0 || i == height - 1) {
+    //         for (int j = 0; j < height; j++) {
+    //             cout << '#';
+    //         }
+    //     }
+    //     else {
+    //         for (int j = 0; j < height; j++) {
+    //             // if (j > 0 && j < height - 1) {
+    //             //     cout << ' ';
+    //             // }
+    //             // else {
+    //             //     cout << '#';
+    //             // }
+
+    //             if (j == 0 || j == height - 1) {
+    //                 cout << '#';
+    //             }
+    //             else {
+    //                 if (i > 1 && i < height - 1) {
+
+    //                 }
+    //                 cout << '*';
+    //             }
+    //         }
+    //     }
+    //     cout << '\n';  
+    // }
 }
